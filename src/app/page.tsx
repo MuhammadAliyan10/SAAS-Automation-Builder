c21af1cf4c6f4d39ee29bd59019ab78e718dd3b5
MuhammadAliyan10 <amfminus@gmail.com>
2024-09-05 10:17:03 +0500

    Main installation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Global/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+     <Navbar/>
+     
+    </div>
+  );
+}
